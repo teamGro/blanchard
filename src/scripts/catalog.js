71d1359c332b1e0aac3e827c4cd9d5303eb1a6f9
Tima <bredos11@yandex.ru>
2021-05-25 14:37:20 +0300

    fixed

--- src/scripts/catalog.js
+++ src/scripts/catalog.js
@@ -1,73 +1,79 @@
-let catalogs = {}
+let catalogs = {};
 
 const personsData = [
-  {
-    img: '',
-    name: 'Бенедетто ди Биндо',
-    date: '2 июня 1448 — 11 января 1494',
-    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ex maiores necessitatibus accusamus deleniti magni quis incidunt corrupti voluptatibus nemo facere at unde, nobis non mollitia optio reprehenderit deserunt velit repudiandae nam! Id tenetur voluptatem iste saepe porro repellendus similique atque laboriosam officiis obcaecati? Dolorem illo iure dolore laborum praesentium atque necessitatibus similique labore ullam. Necessitatibus ad molestiae, itaque molestias, ut quas nam fuga aut delectus nostrum, illum voluptates reprehenderit commodi animi consequuntur debitis suscipit! Consequatur repellendus qui rem deserunt quasi fugiat consequuntur officiis ipsa?`,
-    attr: 'diBindo',
-    alt: 'Бенедетто ди Биндо'
-  },
-  {
-    img: '../img/catalog-domeniko.jpg',
-    name: 'Доменико Гирландайо',
-    date: '2 июня 1448 — 11 января 1494',
-    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ex maiores necessitatibus accusamus deleniti magni quis incidunt corrupti voluptatibus nemo facere at unde, nobis non mollitia optio reprehenderit deserunt velit repudiandae nam! Id tenetur voluptatem iste saepe porro repellendus similique atque laboriosam officiis obcaecati? Dolorem illo iure dolore laborum praesentium atque necessitatibus similique labore ullam. Necessitatibus ad molestiae, itaque molestias, ut quas nam fuga aut delectus nostrum, illum voluptates reprehenderit commodi animi consequuntur debitis suscipit! Consequatur repellendus qui rem deserunt quasi fugiat consequuntur officiis ipsa?`,
-    attr: 'girlandaio',
-    alt: 'Доменико Гирландайо'
-  },
-  {
-    img: '../img/catalog-verrokkio.jpg',
-    name: 'Андреа Верроккьо',
-    date: '2 июня 1448 — 11 января 1494',
-    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ex maiores necessitatibus accusamus deleniti magni quis incidunt corrupti voluptatibus nemo facere at unde, nobis non mollitia optio reprehenderit deserunt velit repudiandae nam! Id tenetur voluptatem iste saepe porro repellendus similique atque laboriosam officiis obcaecati? Dolorem illo iure dolore laborum praesentium atque necessitatibus similique labore ullam. Necessitatibus ad molestiae, itaque molestias, ut quas nam fuga aut delectus nostrum, illum voluptates reprehenderit commodi animi consequuntur debitis suscipit! Consequatur repellendus qui rem deserunt quasi fugiat consequuntur officiis ipsa?`,
-    attr: 'verrokkio',
-    alt: 'Андреа Верроккьо'
-  }
+	{
+		img: "",
+		name: "Бенедетто ди Биндо",
+		date: "2 июня 1448 — 11 января 1494",
+		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ex maiores necessitatibus accusamus deleniti magni quis incidunt corrupti voluptatibus nemo facere at unde, nobis non mollitia optio reprehenderit deserunt velit repudiandae nam! Id tenetur voluptatem iste saepe porro repellendus similique atque laboriosam officiis obcaecati? Dolorem illo iure dolore laborum praesentium atque necessitatibus similique labore ullam. Necessitatibus ad molestiae, itaque molestias, ut quas nam fuga aut delectus nostrum, illum voluptates reprehenderit commodi animi consequuntur debitis suscipit! Consequatur repellendus qui rem deserunt quasi fugiat consequuntur officiis ipsa?`,
+		attr: "diBindo",
+		alt: "Бенедетто ди Биндо",
+	},
+	{
+		img: "../img/catalog-domeniko.jpg",
+		name: "Доменико Гирландайо",
+		date: "2 июня 1448 — 11 января 1494",
+		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ex maiores necessitatibus accusamus deleniti magni quis incidunt corrupti voluptatibus nemo facere at unde, nobis non mollitia optio reprehenderit deserunt velit repudiandae nam! Id tenetur voluptatem iste saepe porro repellendus similique atque laboriosam officiis obcaecati? Dolorem illo iure dolore laborum praesentium atque necessitatibus similique labore ullam. Necessitatibus ad molestiae, itaque molestias, ut quas nam fuga aut delectus nostrum, illum voluptates reprehenderit commodi animi consequuntur debitis suscipit! Consequatur repellendus qui rem deserunt quasi fugiat consequuntur officiis ipsa?`,
+		attr: "girlandaio",
+		alt: "Доменико Гирландайо",
+	},
+	{
+		img: "../img/catalog-verrokkio.jpg",
+		name: "Андреа Верроккьо",
+		date: "2 июня 1448 — 11 января 1494",
+		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ex maiores necessitatibus accusamus deleniti magni quis incidunt corrupti voluptatibus nemo facere at unde, nobis non mollitia optio reprehenderit deserunt velit repudiandae nam! Id tenetur voluptatem iste saepe porro repellendus similique atque laboriosam officiis obcaecati? Dolorem illo iure dolore laborum praesentium atque necessitatibus similique labore ullam. Necessitatibus ad molestiae, itaque molestias, ut quas nam fuga aut delectus nostrum, illum voluptates reprehenderit commodi animi consequuntur debitis suscipit! Consequatur repellendus qui rem deserunt quasi fugiat consequuntur officiis ipsa?`,
+		attr: "verrokkio",
+		alt: "Андреа Верроккьо",
+	},
 ];
 
 const personElems = {
-  personImg: $('.person__img'),
-  personName: $('.person__name'),
-  personDate: $('.person__date'),
-  personStory: $('.person__story')
+	personImg: $(".person__img"),
+	personName: $(".person__name"),
+	personDate: $(".person__date"),
+	personStory: $(".person__story"),
 };
 
 function updateMarkup(elems, data) {
-  elems.personImg.prop('src', data.img || '');
-  elems.personImg.prop('alt', data.alt || '');
-  elems.personName.text(data.name);
-  elems.personDate.text(data.date);
-  elems.personStory.text(data.text)
+	if (!data) {
+		data = personsData[Math.floor(Math.random() * personsData.length)];
+	}
+
+	elems.personImg.prop("src", data.img || "");
+	elems.personImg.prop("alt", data.alt || "");
+	elems.personName.text(data.name);
+	elems.personDate.text(data.date);
+	elems.personStory.text(data.text);
 }
 
 catalogs.showPersonByClick = function (list) {
-  list.on('click', function (e) {
-    let $target = $(e.target);
-    if ($target.prop('tagName') != 'LI') return;
+	list.on("click", function (e) {
+		let $target = $(e.target);
+		if ($target.prop("tagName") != "SPAN") return;
 
-    let dataID = $target.attr('data-id');
-    let person = personsData.filter(item => {
-      return item.attr == dataID;
-    });
+		$target = $target.parent();
+		let dataID = $target.attr("data-id");
+		let person = personsData.filter((item) => {
+			return item.attr == dataID;
+		});
 
-    console.log(person);
-    updateMarkup(personElems, person[0]);
-  })
-}
+		updateMarkup(personElems, person[0]);
+		list.find(".catalog__item-text_active").removeClass("catalog__item-text_active");
+		$target.first().addClass("catalog__item-text_active");
+	});
+};
 
 catalogs.markActiveTab = function (list) {
-  list.on('click', function (e) {
-    let target = $(e.target);
+	list.on("click", function (e) {
+		let target = $(e.target);
 
-    if (target.prop('tagName') != 'BUTTON') return;
+		if (target.prop("tagName") != "BUTTON") return;
 
-    if (target.hasClass('catalog__nav-btn_active')) return;
+		if (target.hasClass("catalog__nav-btn_active")) return;
 
-    target.closest('ul').find('.catalog__nav-btn_active').removeClass('catalog__nav-btn_active');
-    target.addClass('catalog__nav-btn_active');
-  })
-}
+		target.closest("ul").find(".catalog__nav-btn_active").removeClass("catalog__nav-btn_active");
+		target.addClass("catalog__nav-btn_active");
+	});
+};
 
 export default catalogs;
