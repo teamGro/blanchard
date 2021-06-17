0401a66afd260dc4fa283a6f685879b0753c7730
Tima <bredos11@yandex.ru>
2021-06-17 16:22:18 +0300

    fixed

--- src/scripts/catalog.js
+++ src/scripts/catalog.js
@@ -1,6 +1,6 @@
 let catalogs = {};
 
-const personsData = [
+catalogs.personsData = [
 	// {
 	// 	img: "",
 	// 	name: "Бенедетто ди Биндо",
@@ -10,133 +10,131 @@ const personsData = [
 	// 	alt: "Бенедетто ди Биндо",
 	// },
 	{
-		img: "../img/catalog-domeniko.jpg",
-		name: "Доменико Гирландайо",
-		date: "2 июня 1448 — 11 января 1494",
+		img: './img/catalog-domeniko.jpg',
+		name: 'Доменико Гирландайо',
+		date: '2 июня 1448 — 11 января 1494',
 		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ex maiores necessitatibus accusamus deleniti magni quis incidunt corrupti voluptatibus nemo facere at unde, nobis non mollitia optio reprehenderit deserunt velit repudiandae nam! Id tenetur voluptatem iste saepe porro repellendus similique atque laboriosam officiis obcaecati? Dolorem illo iure dolore laborum praesentium atque necessitatibus similique labore ullam. Necessitatibus ad molestiae, itaque molestias, ut quas nam fuga aut delectus nostrum, illum voluptates reprehenderit commodi animi consequuntur debitis suscipit! Consequatur repellendus qui rem deserunt quasi fugiat consequuntur officiis ipsa?`,
-		attr: "girlandaio",
-		alt: "Доменико Гирландайо",
+		attr: 'girlandaio',
+		alt: 'Доменико Гирландайо',
 	},
 	{
-		img: "../img/catalog-verrokkio.jpg",
-		name: "Андреа Верроккьо",
-		date: "2 июня 1448 — 11 января 1494",
+		img: './img/catalog-verrokkio.jpg',
+		name: 'Андреа Верроккьо',
+		date: '2 июня 1448 — 11 января 1494',
 		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ex maiores necessitatibus accusamus deleniti magni quis incidunt corrupti voluptatibus nemo facere at unde, nobis non mollitia optio reprehenderit deserunt velit repudiandae nam! Id tenetur voluptatem iste saepe porro repellendus similique atque laboriosam officiis obcaecati? Dolorem illo iure dolore laborum praesentium atque necessitatibus similique labore ullam. Necessitatibus ad molestiae, itaque molestias, ut quas nam fuga aut delectus nostrum, illum voluptates reprehenderit commodi animi consequuntur debitis suscipit! Consequatur repellendus qui rem deserunt quasi fugiat consequuntur officiis ipsa?`,
-		attr: "verrokkio",
-		alt: "Андреа Верроккьо",
+		attr: 'verrokkio',
+		alt: 'Андреа Верроккьо',
 	},
 	{
-		img: "../img/french-01.jpg",
-		name: "Дюмустье, Жоффруа",
-		date: "2 июня 1448 — 11 января 1494",
+		img: './img/french-01.jpg',
+		name: 'Дюмустье, Жоффруа',
+		date: '2 июня 1448 — 11 января 1494',
 		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ex maiores necessitatibus accusamus deleniti magni quis incidunt corrupti voluptatibus nemo facere at unde, nobis non mollitia optio reprehenderit deserunt velit repudiandae nam! Id tenetur voluptatem iste saepe porro repellendus similique atque laboriosam officiis obcaecati? Dolorem illo iure dolore laborum praesentium atque necessitatibus similique labore ullam. Necessitatibus ad molestiae, itaque molestias, ut quas nam fuga aut delectus nostrum, illum voluptates reprehenderit commodi animi consequuntur debitis suscipit! Consequatur repellendus qui rem deserunt quasi fugiat consequuntur officiis ipsa?`,
-		attr: "joffrua",
-		alt: "Дюмустье, Жоффруа",
+		attr: 'joffrua',
+		alt: 'Дюмустье, Жоффруа',
 	},
 	{
-		img: "../img/french-02.png",
-		name: "Косм II Дюмустье",
-		date: " 1545 — 1605",
+		img: './img/french-02.png',
+		name: 'Косм II Дюмустье',
+		date: ' 1545 — 1605',
 		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ex maiores necessitatibus accusamus deleniti magni quis incidunt corrupti voluptatibus nemo facere at unde, nobis non mollitia optio reprehenderit deserunt velit repudiandae nam! Id tenetur voluptatem iste saepe porro repellendus similique atque laboriosam officiis obcaecati? Dolorem illo iure dolore laborum praesentium atque necessitatibus similique labore ullam. Necessitatibus ad molestiae, itaque molestias, ut quas nam fuga aut delectus nostrum, illum voluptates reprehenderit commodi animi consequuntur debitis suscipit! Consequatur repellendus qui rem deserunt quasi fugiat consequuntur officiis ipsa?`,
-		attr: "dumustie",
-		alt: "Косм II Дюмустье",
+		attr: 'dumustie',
+		alt: 'Косм II Дюмустье',
 	},
 	{
-		img: "../img/german-01.jpg",
-		name: "А́льбрехт Дю́рер",
-		date: " 1545 — 1605",
+		img: './img/german-01.jpg',
+		name: 'А́льбрехт Дю́рер',
+		date: ' 1545 — 1605',
 		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ex maiores necessitatibus accusamus deleniti magni quis incidunt corrupti voluptatibus nemo facere at unde, nobis non mollitia optio reprehenderit deserunt velit repudiandae nam! Id tenetur voluptatem iste saepe porro repellendus similique atque laboriosam officiis obcaecati? Dolorem illo iure dolore laborum praesentium atque necessitatibus similique labore ullam. Necessitatibus ad molestiae, itaque molestias, ut quas nam fuga aut delectus nostrum, illum voluptates reprehenderit commodi animi consequuntur debitis suscipit! Consequatur repellendus qui rem deserunt quasi fugiat consequuntur officiis ipsa?`,
-		attr: "durer",
-		alt: "А́льбрехт Дю́рер",
+		attr: 'durer',
+		alt: 'А́льбрехт Дю́рер',
 	},
 	{
-		img: "../img/german-02.jpg",
-		name: "Эльсхаймер, Адам",
-		date: " 1545 — 1605",
+		img: './img/german-02.jpg',
+		name: 'Эльсхаймер, Адам',
+		date: ' 1545 — 1605',
 		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ex maiores necessitatibus accusamus deleniti magni quis incidunt corrupti voluptatibus nemo facere at unde, nobis non mollitia optio reprehenderit deserunt velit repudiandae nam! Id tenetur voluptatem iste saepe porro repellendus similique atque laboriosam officiis obcaecati? Dolorem illo iure dolore laborum praesentium atque necessitatibus similique labore ullam. Necessitatibus ad molestiae, itaque molestias, ut quas nam fuga aut delectus nostrum, illum voluptates reprehenderit commodi animi consequuntur debitis suscipit! Consequatur repellendus qui rem deserunt quasi fugiat consequuntur officiis ipsa?`,
-		attr: "adam",
-		alt: "Эльсхаймер, Адам",
+		attr: 'adam',
+		alt: 'Эльсхаймер, Адам',
 	},
 	{
-		img: "../img/russian-01.jpg",
-		name: "Иван Никитич Никитин",
-		date: " 1545 — 1605",
+		img: './img/russian-01.jpg',
+		name: 'Иван Никитич Никитин',
+		date: ' 1545 — 1605',
 		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ex maiores necessitatibus accusamus deleniti magni quis incidunt corrupti voluptatibus nemo facere at unde, nobis non mollitia optio reprehenderit deserunt velit repudiandae nam! Id tenetur voluptatem iste saepe porro repellendus similique atque laboriosam officiis obcaecati? Dolorem illo iure dolore laborum praesentium atque necessitatibus similique labore ullam. Necessitatibus ad molestiae, itaque molestias, ut quas nam fuga aut delectus nostrum, illum voluptates reprehenderit commodi animi consequuntur debitis suscipit! Consequatur repellendus qui rem deserunt quasi fugiat consequuntur officiis ipsa?`,
-		attr: "nikitin",
-		alt: "Иван Никитич Никитин",
+		attr: 'nikitin',
+		alt: 'Иван Никитич Никитин',
 	},
 	{
-		img: "../img/russian-02.jpg",
-		name: "Иван Константинович Айвазовский",
-		date: " 1545 — 1605",
+		img: './img/russian-02.jpg',
+		name: 'Иван Константинович Айвазовский',
+		date: ' 1545 — 1605',
 		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ex maiores necessitatibus accusamus deleniti magni quis incidunt corrupti voluptatibus nemo facere at unde, nobis non mollitia optio reprehenderit deserunt velit repudiandae nam! Id tenetur voluptatem iste saepe porro repellendus similique atque laboriosam officiis obcaecati? Dolorem illo iure dolore laborum praesentium atque necessitatibus similique labore ullam. Necessitatibus ad molestiae, itaque molestias, ut quas nam fuga aut delectus nostrum, illum voluptates reprehenderit commodi animi consequuntur debitis suscipit! Consequatur repellendus qui rem deserunt quasi fugiat consequuntur officiis ipsa?`,
-		attr: "aivaz",
-		alt: "Иван Константинович Айвазовский",
+		attr: 'aivaz',
+		alt: 'Иван Константинович Айвазовский',
 	},
 	{
-		img: "../img/belgian-01.jpg",
-		name: "Антонис (Антон) ван Дейк",
-		date: " 1545 — 1605",
+		img: './img/belgian-01.jpg',
+		name: 'Антонис (Антон) ван Дейк',
+		date: ' 1545 — 1605',
 		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ex maiores necessitatibus accusamus deleniti magni quis incidunt corrupti voluptatibus nemo facere at unde, nobis non mollitia optio reprehenderit deserunt velit repudiandae nam! Id tenetur voluptatem iste saepe porro repellendus similique atque laboriosam officiis obcaecati? Dolorem illo iure dolore laborum praesentium atque necessitatibus similique labore ullam. Necessitatibus ad molestiae, itaque molestias, ut quas nam fuga aut delectus nostrum, illum voluptates reprehenderit commodi animi consequuntur debitis suscipit! Consequatur repellendus qui rem deserunt quasi fugiat consequuntur officiis ipsa?`,
-		attr: "vanDeyk",
-		alt: "Антонис (Антон) ван Дейк",
+		attr: 'vanDeyk',
+		alt: 'Антонис (Антон) ван Дейк',
 	},
 	{
-		img: "../img/belgian-02.jpg",
-		name: "Галле, Луи",
-		date: " 1545 — 1605",
+		img: './img/belgian-02.jpg',
+		name: 'Галле, Луи',
+		date: ' 1545 — 1605',
 		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ex maiores necessitatibus accusamus deleniti magni quis incidunt corrupti voluptatibus nemo facere at unde, nobis non mollitia optio reprehenderit deserunt velit repudiandae nam! Id tenetur voluptatem iste saepe porro repellendus similique atque laboriosam officiis obcaecati? Dolorem illo iure dolore laborum praesentium atque necessitatibus similique labore ullam. Necessitatibus ad molestiae, itaque molestias, ut quas nam fuga aut delectus nostrum, illum voluptates reprehenderit commodi animi consequuntur debitis suscipit! Consequatur repellendus qui rem deserunt quasi fugiat consequuntur officiis ipsa?`,
-		attr: "galle",
-		alt: "Галле, Луи",
+		attr: 'galle',
+		alt: 'Галле, Луи',
 	},
 ];
 
-const personElems = {
-	personImg: $(".person__img"),
-	personName: $(".person__name"),
-	personDate: $(".person__date"),
-	personStory: $(".person__story"),
+catalogs.personElems = {
+	personImg: $('.person__img'),
+	personName: $('.person__name'),
+	personDate: $('.person__date'),
+	personStory: $('.person__story'),
 };
 
-function updateMarkup(elems, data) {
+catalogs.updateMarkup = function (elems, data) {
 	if (!data) {
-		data = personsData[Math.floor(Math.random() * personsData.length)];
+		data = catalogs.personsData[Math.floor(Math.random() * catalogs.personsData.length)];
 	}
-
-	elems.personImg.prop("src", data.img || "");
-	elems.personImg.prop("alt", data.alt || "");
+	elems.personImg.prop('src', data.img || '');
+	elems.personImg.prop('alt', data.alt || '');
 	elems.personName.text(data.name);
 	elems.personDate.text(data.date);
 	elems.personStory.text(data.text);
-}
+};
 
 catalogs.showPersonByClick = function (list) {
-	list.on("click", function (e) {
+	list.on('click', function (e) {
 		let $target = $(e.target);
-		if ($target.prop("tagName") != "BUTTON" || $target.hasClass("accordion-button")) return;
+		if ($target.prop('tagName') != 'BUTTON' || $target.hasClass('accordion-button')) return;
 
-		$target = $target.parent();
-		let dataID = $target.attr("data-id");
-		let person = personsData.filter((item) => {
+		let dataID = $target.attr('data-id');
+		let person = catalogs.personsData.filter((item) => {
 			return item.attr == dataID;
 		});
 
-		updateMarkup(personElems, person[0]);
-		list.find(".catalog__item-text_active").removeClass("catalog__item-text_active");
-		$target.first().addClass("catalog__item-text_active");
+		catalogs.updateMarkup(catalogs.personElems, person[0]);
+		list.find('.catalog__item-text_active').removeClass('catalog__item-text_active');
+		$target.first().addClass('catalog__item-text_active');
 	});
 };
 
 catalogs.markActiveTab = function (list) {
-	list.on("click", function (e) {
+	list.on('click', function (e) {
 		let target = $(e.target);
 
-		if (target.prop("tagName") != "BUTTON") return;
+		if (target.prop('tagName') != 'BUTTON') return;
 
-		if (target.hasClass("catalog__nav-btn_active")) return;
+		if (target.hasClass('catalog__nav-btn_active')) return;
 
-		target.closest("ul").find(".catalog__nav-btn_active").removeClass("catalog__nav-btn_active");
-		target.addClass("catalog__nav-btn_active");
+		target.closest('ul').find('.catalog__nav-btn_active').removeClass('catalog__nav-btn_active');
+		target.addClass('catalog__nav-btn_active');
 	});
 };
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,33 +1,47 @@
-import gallerySwiperSettings from "./gallery";
-const select = document.querySelector("#galleryFilter");
+import gallerySwiperSettings from './gallery';
+const select = document.querySelector('#galleryFilter');
 new Choices(select, {
 	searchEnabled: false,
-	itemSelectText: "",
+	itemSelectText: '',
 	// shouldSort: false,
 	// maxItemCount: 3,
 	// placeholder: false,
 });
 
-let gallerySwiper = new Swiper("#gallerySwiper", gallerySwiperSettings);
+let gallerySwiper = new Swiper('#gallerySwiper', gallerySwiperSettings);
 
-import dropDownBtnHandler from "./header";
+import dropDownBtnHandler from './header';
 dropDownBtnHandler();
 
-import catalogs from "./catalog";
-const $personList = $("#accordionPersons");
+import catalogs from './catalog';
+const $personList = $('#accordionPersons');
 catalogs.showPersonByClick($personList);
 
-const $personListFrench = $("#accordionPersonsFrench");
+const $personListFrench = $('#accordionPersonsFrench');
 catalogs.showPersonByClick($personListFrench);
 
-const $personListGerman = $("#accordionPersonsGerman");
+const $personListGerman = $('#accordionPersonsGerman');
 catalogs.showPersonByClick($personListGerman);
 
-const $personListRussian = $("#accordionPersonsRussian");
+const $personListRussian = $('#accordionPersonsRussian');
 catalogs.showPersonByClick($personListRussian);
 
-const $personListBelgian = $("#accordionPersonsBelgian");
+const $personListBelgian = $('#accordionPersonsBelgian');
 catalogs.showPersonByClick($personListBelgian);
 
-const $catalogNavs = $("#catalogNavs");
+const $catalogNavs = $('#catalogNavs');
 catalogs.markActiveTab($catalogNavs);
+
+$('.catalog__nav-btn').each(function () {
+	$(this).on('click', function (e) {
+		let target = $(e.target);
+
+		let btnID = $(target).attr('id');
+		let tabContainerContent = $(`[data-target=${btnID}]`).find('.catalog__accordion');
+		let activePerson = tabContainerContent.find('.catalog__item-text_active').attr('data-id');
+		let person = catalogs.personsData.filter((item) => {
+			return item.attr == activePerson;
+		});
+		catalogs.updateMarkup(catalogs.personElems, person[0]);
+	});
+});
